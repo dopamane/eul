d5c32c338951e188453fe676d0dbd3894bd21933
dopamane <dwc1295@gmail.com>
2019-05-30 02:13:29 -0400

    use spi worker

--- src/Eul.hs
+++ src/Eul.hs
@@ -3,39 +3,22 @@ module Eul where
 
 import Clash.Prelude
 
-import Rstn (rstn)
+import Rstn ( rstn )
+import Spi  ( spiWorker )
 
-import Control.Lens ( makeLenses, to, use, (^.), (.=), (%=) )
-import Control.Monad
-import Control.Monad.State
-
-import qualified Data.List as L
-
-type DomSpi = 'Dom "Spi" 166667 -- freqCalc 6e6  (6mhz)
+--import qualified Data.List as L
 
 data Op a
   = Add a a
   | Sub a a
 
-data Spi r t
-  = Spi { _rx  :: Vec r Bit
-        , _tx  :: Vec t Bit
-        , _ss' :: Bool
-        }
-makeLenses ''Spi
-
 data Stage = Read | Write
-
-data Eul r t
-  = Eul { _stage :: Stage
-        , _spi   :: Spi r t
-        }
-makeLenses ''Eul
 
 {-# ANN topEntity
   (Synthesize
     { t_name = "EUL"
-    , t_inputs = [ PortName "SCK"
+    , t_inputs = [ PortName "clk"
+                 , PortName "SCK"
                  , PortName "SS"
                  , PortName "MOSI"
                  ]
@@ -43,50 +26,36 @@ makeLenses ''Eul
     })#-}
 
 topEntity
-  :: Clock DomSpi 'Source -- sck
-  -> Signal DomSpi Bool   -- ss
-  -> Signal DomSpi Bit    -- mosi
-  -> Signal DomSpi Bit    -- miso
-topEntity sck = withClockReset sck rst eul
+  :: Clock System 'Source -- clk
+  -> Signal System Bit    -- sck
+  -> Signal System Bool   -- ss
+  -> Signal System Bit    -- mosi
+  -> Signal System Bit    -- miso
+topEntity clk = withClockReset clk rst eul
   where
-    rst = rstn d100 sck
+    rst = rstn d16 clk
 {-# NOINLINE topEntity #-}
 
 eul
   :: HiddenClockReset dom gated sync
-  => Signal dom Bool
+  => Signal dom Bit
+  -> Signal dom Bool
   -> Signal dom Bit
   -> Signal dom Bit
-eul ss mosi = eulO <$> eulReg
+eul sck ss mosi = miso
   where
-    eulReg = register initial $ eulT <$> eulReg <*> ss <*> mosi
-    initial = Eul Read (Spi (replicate d24 0) (replicate d16 0) True)
+    (miso, ack, rx) = spiWorker txLd sck ss mosi
+    txLd = mealyB eulT initial (ack, rx)
+    initial = (Read, 0 :: BitVector 16)
 
-eulO :: Eul r (t+1) -> Bit
-eulO s = s^.spi.tx.to head
-
-eulT :: Eul 24 16 -> Bool -> Bit -> Eul 24 16
-eulT s ss mosi = flip execState s $ do
-  case s^.stage of
-    Read  -> spiRead ss mosi
-    Write -> spiWrite ss
-  spi.ss' .= ss
-
-spiRead :: Bool -> Bit -> State (Eul 24 16) ()
-spiRead ss mosi = do
-  s <- use spi
-  if s^.ss'.to not && ss
-    then do
-      stage .= Write
-      spi.tx .= calc (s^.rx.to v2bv)
-    else unless ss $ spi.rx %= (<<+ mosi)
-
-spiWrite :: (KnownNat r, KnownNat t) => Bool -> State (Eul r t) ()
-spiWrite ss = do
-  s <- use spi
-  if s^.ss'.to not && ss
-    then put $ Eul Read (Spi (repeat 0) (repeat 0) True)
-    else unless ss $ spi.tx %= flip rotateLeftS d1
+eulT
+  :: (Stage, BitVector 16)
+  -> (Bool, Maybe (BitVector 24))
+  -> ((Stage, BitVector 16), Maybe (BitVector 16))
+eulT (Read, _) (_, Nothing) = ((Read, 0), Nothing)
+eulT (Read, _) (_, Just r) = ((Write, calc r), Nothing)
+eulT (Write, w) (False, _) = ((Write, w), Just w)
+eulT (Write, w) (True, _)  = ((Read, 0), Just w)
 
 decode :: BitVector 24 -> Maybe (Op (Unsigned 8))
 decode bs = case slice d23 d16 bs of
@@ -105,14 +74,16 @@ execute = \case
 alu :: BitVector 24 -> Maybe (Unsigned 8)
 alu = fmap execute . decode
 
-process :: Maybe (Unsigned 8) -> Vec 16 Bit
-process = bv2v . maybe 0 ((1 ++#) . pack)
+process :: Maybe (Unsigned 8) -> BitVector 16
+process = maybe 0 ((1 ++#) . pack)
 
-calc :: BitVector 24 -> Vec 16 Bit
+calc :: BitVector 24 -> BitVector 16
 calc = process . alu
 
+{-
 eulTest :: [Bit]
 eulTest = sampleN 43 $ eul ss mosi
   where
     ss = fromList $ [True] L.++ L.replicate 24 False L.++ [True, True] L.++ L.replicate 16 False
     mosi = fromList $ [0, 0,0,0,0,0,0,0,1, 0,0,0,0,0,0,1,1, 0,0,0,0,0,0,0,1] L.++ L.replicate 18 0
+-}
