cb8be75dc630abbf7df48bf0d8f554ee1de0c2fc
dopamane <dwc1295@gmail.com>
2019-05-30 16:17:01 -0400

    fetch instructions from rom instead of spi rx

--- src/Eul.hs
+++ src/Eul.hs
@@ -2,12 +2,11 @@ module Eul where
 
 import Clash.Prelude
 
-import Control.Lens ( makeLenses, use, (^.), (.=) )
-import Control.Monad.State 
-import Data.Maybe ( isJust, fromMaybe )
+import Control.Lens ( makeLenses, use, to, (^.), (.=), (+=) )
+import Control.Monad.State
 
 import Rstn ( rstn )
-import Spi  ( spiWorker )
+import Spi  ( spiWorkerTx )
 
 type Addr    = Index 8
 type Reg     = BitVector 32
@@ -20,21 +19,26 @@ data Instr
   | Mul Addr Addr Addr
   | PutH Addr Imm
   | PutL Addr Imm
+  | Beq Addr Addr Addr
   | Get Addr
+  | Nop
 
 -- Add  => 0b000 => 0
 -- Sub  => 0b001 => 1
 -- Mul  => 0b010 => 2
--- putH => 0b011 => 3 
--- putL => 0b100 => 4
--- Get  => 0b101 => 5
+-- PutH => 0b011 => 3
+-- PutL => 0b100 => 4
+-- Beq  => 0b101 => 5
+-- Get  => 0b110 => 6
+-- Nop  => 0b111 => 7
 
 data Stage = Fetch | Execute | Write
 
-data Eul = Eul
+data Eul n = Eul
   { _instr :: Instr
   , _stage :: Stage
   , _regs  :: RegBank
+  , _pc    :: Index n
   }
 makeLenses ''Eul
 
@@ -44,7 +48,6 @@ makeLenses ''Eul
     , t_inputs = [ PortName "clk"
                  , PortName "SCK"
                  , PortName "SS"
-                 , PortName "MOSI"
                  ]
     , t_output = PortName "MISO"
     })#-}
@@ -52,59 +55,58 @@ topEntity
   :: Clock System 'Source -- clk
   -> Signal System Bit    -- sck
   -> Signal System Bool   -- ss
-  -> Signal System Bit    -- mosi
   -> Signal System Bit    -- miso
-topEntity clk = withClockReset clk rst eul
+topEntity clk = withClockReset clk rst (eul prog)
   where
     rst = rstn d16 clk
 {-# NOINLINE topEntity #-}
 
 eul
   :: HiddenClockReset dom gated sync
-  => Signal dom Bit
+  => (KnownNat n, 1 <= n)
+  => Vec n Instr
+  -> Signal dom Bit
   -> Signal dom Bool
   -> Signal dom Bit
-  -> Signal dom Bit
-eul sck ss mosi = miso
+eul romContent sck ss = miso
   where
-    (miso, ack, rx) = spiWorker txLd sck ss mosi
-    txLd = mooreB eulT writeBack initial (ack, rx)
-    initial = Eul (Get 0) Fetch $ repeat 0
+    (miso, ack) = spiWorkerTx txLd sck ss
+    txLd = moore (eulT romContent) writeBack initial ack
+    initial = Eul Nop Fetch (repeat 0) 0
 
-writeBack :: Eul -> Maybe (BitVector 32)
+writeBack :: Eul n -> Maybe (BitVector 32)
 writeBack Eul{_instr=(Get i), _stage=Write, _regs=r} = Just $ r !! i
 writeBack _ = Nothing
 
-eulT
-  :: Eul
-  -> (Bool, Maybe (BitVector 32))
-  -> Eul
-eulT s (ack, rx) = flip execState s $ case s^.stage of
-  Fetch   -> fetch rx
+eulT :: (KnownNat n, 1 <= n) => Vec n Instr -> Eul n -> Bool -> Eul n
+eulT romContent s ack = flip execState s $ case s^.stage of
+  Fetch   -> s^.pc.to (fetch romContent)
   Execute -> execute
-  Write   -> when ack $ stage .= Fetch 
-    
-fetch :: Maybe (BitVector 32) -> State Eul ()
-fetch Nothing = return ()
-fetch (Just rx) = do
-  let decoded = decode rx
-  instr .= fromMaybe (Get 0) decoded
-  when (isJust decoded) $ stage .= Execute  
+  Write   -> when ack $ stage .= Fetch
 
-execute :: State Eul ()
+fetch :: KnownNat n => Vec n Instr -> Index n -> State (Eul n) ()
+fetch romContent instrAddr = do
+  instr .= asyncRom romContent instrAddr
+  stage .= Execute
+
+execute :: (KnownNat n, 1 <= n) => State (Eul n) ()
 execute = do
-  r <- use regs
-  ins <- use instr
+  r     <- use regs
+  ins   <- use instr
+  curPC <- use pc
   regs .= case ins of
     Add  a b c -> writeReg r c $ (r !! a) + (r !! b)
     Sub  a b c -> writeReg r c $ (r !! a) - (r !! b)
     Mul  a b c -> writeReg r c $ (r !! a) * (r !! b)
     PutH a i   -> writeReg r a (i ++# getLower (r !! a))
-    PutL a i   -> writeReg r a (getHigher (r !! a) ++# i) 
+    PutL a i   -> writeReg r a (getHigher (r !! a) ++# i)
     _          -> r
   stage .= case ins of
     Get _ -> Write
     _     -> Fetch
+  case ins of
+    Beq a b pcRegAddr | (r !! a) == (r !! b) -> pc .= unpack (resize $ r !! pcRegAddr)
+    _  -> unless (curPC == maxBound) $ pc += 1
   where
     getHigher = slice d31 d16
     getLower  = slice d15 d0
@@ -118,7 +120,7 @@ writeReg r i d = replace i d r
 
 --   31   28   25   22   19    15               0
 -- 0b[***][***][***][***][****][****************]
--- opcode|adr1|adr2|adr3|unused|immediate 
+-- opcode|adr1|adr2|adr3|unused|immediate
 
 decode :: BitVector 32 -> Maybe Instr
 decode bs = case slice d31 d29 bs of
@@ -127,13 +129,23 @@ decode bs = case slice d31 d29 bs of
   2 -> Just $ Mul  addr1 addr2 addr3
   3 -> Just $ PutH addr1 imm
   4 -> Just $ PutL addr1 imm
-  5 -> Just $ Get  addr1 
+  5 -> Just $ Beq  addr1 addr2 addr3
+  6 -> Just $ Get  addr1
+  7 -> Just Nop
   _ -> Nothing
   where
     addr1 = unpack $ slice d28 d26 bs
     addr2 = unpack $ slice d25 d23 bs
     addr3 = unpack $ slice d22 d20 bs
     imm   = slice d15 d0 bs
+
+prog :: Vec 5 Instr
+prog =  PutL 0 5
+     :> PutL 1 7
+     :> Add  0 1 2
+     :> Get  2
+     :> Nop
+     :> Nil
 
 {-
 sample program using narco:
