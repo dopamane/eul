25838a41a9e7ba371260ed597b2243cd82e4ef0b
dopamane <dwc1295@gmail.com>
2019-06-01 15:11:05 -0400

    testing fib

--- src/Eul.hs
+++ src/Eul.hs
@@ -199,7 +199,7 @@ prog =  Nop
      :> Nop
      :> Nil ++ jmpBegin
 
-fib :: Vec 14 (Instr 8 10)
+fib :: Vec 16 (Instr 8 10)
 fib =  Nop
     :> LoadIL 0 29 -- nth  fibonacci number 10 -> r0
     :> LoadIL 1 0  -- prev prev              0 -> r1
@@ -214,6 +214,8 @@ fib =  Nop
     :> Mov 7 3   --                       r7 -> r3
     :> Bne 3 0 5 -- goto LOOP BEGIN if i /= n
     :> Get 2     -- spi write
+    :> Nop
+    :> Nop
     :> Nil
 
 ramTest :: Vec 13 (Instr 8 10)
