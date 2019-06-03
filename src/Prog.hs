ac8acb80d3bbc9efe5c0126f74fca330d92775eb
dopamane <dwc1295@gmail.com>
2019-06-03 11:34:26 -0400

    use .eul programs as programmer input to Eul chip

--- src/Prog.hs
+++ src/Prog.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+import System.Environment ( getArgs )
 import Control.Monad      ( forM_ )
 import Control.Concurrent ( threadDelay )
 import Data.Word          ( Word8, Word16 )
@@ -21,28 +22,15 @@ data Instr
   | Load  Addr Addr -- r2 = mem addr
   | Store Addr Addr -- r1 = value, r2 = mem addr
   | Nop
-
-progMult :: [Instr]
-progMult
-  =  [ ImmL 0 5
-     , ImmL 1 5
-     , Mul  0 1 2
-     , Get 2
-     ]
-
-progAdd :: [Instr]
-progAdd
-  = [ ImmL 0 4
-    , ImmL 1 4
-    , Add 0 1 2
-    , Get 2
-    ]
+  deriving Show
 
 main :: IO ()
 main = do
-  writeBulkByte ([0x00, 0x00] ++ encodeWord16 (fromIntegral $ length progMult)) -- write prog size
+  [fileName] <- getArgs
+  prog <- parseProg <$> readFile fileName
+  writeBulkByte ([0x00, 0x00] ++ encodeWord16 (fromIntegral $ length prog)) -- write prog size
   threadDelay 1000
-  forM_ progMult $ \instr -> do -- write prog instructions
+  forM_ prog $ \instr -> do -- write prog instructions
     writeBulkByte (encodeInstr instr)
     threadDelay 1000
   threadDelay 1000
@@ -110,3 +98,21 @@ encodeWord16 x = map fromIntegral [(x .&. 0xFF00) `shiftR` 8, x .&. 0xFF]
 
 parseImm :: Word16 -> [Word8]
 parseImm = encodeWord16
+
+parseInstr :: [String] -> Instr
+parseInstr = \case
+  ["Add", a, b, c] -> Add (read a) (read b) (read c)
+  ["Sub", a, b, c] -> Sub (read a) (read b) (read c)
+  ["Mul", a, b, c] -> Mul (read a) (read b) (read c)
+  ["ImmH", a, i]   -> ImmH (read a) (read i)
+  ["ImmL", a, i]   -> ImmL (read a) (read i)
+  ["Bne", a, b, c] -> Bne (read a) (read b) (read c)
+  ["Mov", a, b]    -> Mov (read a) (read b)
+  ["Get", a]       -> Get (read a)
+  ["Put", a]       -> Put (read a)
+  ["Load", a, b]   -> Load (read a) (read b)
+  ["Store", a, b]  -> Store (read a) (read b)
+  _                -> Nop
+
+parseProg :: String -> [Instr]
+parseProg = map (parseInstr . words) . lines
